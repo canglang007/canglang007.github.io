64f8e9cc6573d4bd7ed8f6e8703148d06498c529
Zhuoming Liu <2097254505@qq.com>
2025-03-14 00:05:56 +0800

    更新内容

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,14 @@
 hexo clean
 hexo generate
 git add .
-git commit -m "更新内容"
+default_msg="更新内容"
+
+# 获取用户输入
+read -p "请输入提交说明（默认: $default_msg）: " user_msg
+
+# 使用默认值或用户输入
+commit_msg="${user_msg:-$default_msg}"
+
+git commit -m "$commit_msg"
 git push origin main
 hexo deploy
